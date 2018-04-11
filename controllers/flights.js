657b653f30df00766807f3cf762f280b009be277
Angela Ahn <aahn84@gmail.com>
2018-04-11 11:46:18 -0700

    update flights route

--- controllers/flights.js
+++ controllers/flights.js
@@ -23,7 +23,7 @@ function getFlightById(req, res, next) {
 function getFlightInfo(req, res, next) {
   const id = req.params.id;
   const { airline_name, flight_num, depart_date } = req.body;
-  return model.flights.getFlight(airline_name, flight_num, depart_date, id)
+  return model.flights.getFlightInfo(airline_name, flight_num, depart_date, id)
     .then(flight => {
       return res.status(200).json({ data: flight });
     })
