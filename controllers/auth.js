e64ce29e7575aeb8f1873f1f631cb1fb25d4d5d1
Angela Ahn <aahn84@gmail.com>
2018-04-16 00:39:43 -0700

    work on auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -18,11 +18,13 @@ function signup(req, res, next) {
 
 function login(req, res, next) {
   const { email, password } = req.body;
+  console.log('BODY', req.body);
   if (!email || !password) {
     return next({ status: 400, message: 'Missing fields.' });
   }
   return model.users.login(email, password)
     .then(token => {
+      console.log('TOKEN', token);
       return res.set('Auth', `Bearer: ${token}`)
         .send({ message: 'Login successful.' });
     })
--- routes/flights.js
+++ routes/flights.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const controller = require('../controllers');
+const authorize = require('./authMiddleware');
 
 const router = express.Router();
 
 router.get('/', controller.flights.getAllFlights);
-router.get('/:id', controller.flights.getFlightById);
+router.get('/:id', authorize, controller.flights.getFlightById);
 router.post('/:id', controller.flights.getFlightInfo);
-router.delete('/:id', controller.flights.deleteFlight)
+router.delete('/:id', authorize, controller.flights.deleteFlight)
 
 module.exports = router;
