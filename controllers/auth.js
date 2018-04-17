52829e2ab49a3681cdb5934e973201b50b6866e2
Angela Ahn <aahn84@gmail.com>
2018-04-16 18:51:00 -0700

    remove debugger and console logs

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,17 @@
 const model = require('../models');
 
 function signup(req, res, next) {
-  console.log('in signup')
-  debugger
   const { first_name, last_name, email, password } = req.body;
-  console.log('BODY', req.body);
   if (!first_name || !last_name || !email || !password) {
     return next({ status: 400, message: 'Missing fields.' });
   }
   //return model.users.signup(first_name, last_name, email, password)
   return model.users.signup(req.body)
     .then(token => {
-      debugger
       return res.set('Auth', `Bearer: ${token}`)
         .send({ message: 'Sign up successful.' });
     })
     .catch(err => {
-      debugger
       console.log('ERROR: ', err);
       return next({ status: 401, message: 'Error creating user.' });
     });
